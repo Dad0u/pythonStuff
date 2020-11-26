4a87912742ac163cc953953b79e9ae95e83e8833
Victor Couty <victor@couty.eu>
2020-11-26 15:27:28 +0100

    Added support for multiple args in cachedfunc

--- cachedfunc.py
+++ cachedfunc.py
@@ -4,33 +4,38 @@ import pickle
 def cachedfunc(savefile):
   # Decorator to save the results of a function
   # Allows the result to be loaded instead of re-computed
-  # Only for funcs with a single hashable argument
+  # Only for funcs with hashable arguments !
   # Result mut be picklable
   def deco(f):
-    def newf(arg):
+    def newf(*args):
+      args = tuple(args)
       try:
         with open(savefile,'rb') as sf:
           saved = pickle.load(sf)
       except FileNotFoundError:
         saved = {}
-      if arg not in saved:
-        print(f"[cachedfunc] {f.__name__}({arg}) not found, adding...")
-        saved[arg] = f(arg)
+      if args not in saved:
+        print(f"[cachedfunc] {f.__name__}{args} not found, adding...")
+        saved[args] = f(*args)
         with open(savefile,'wb') as sf:
           pickle.dump(saved,sf)
       else:
-        print(f"[cachedfunc] Using cached value for {f.__name__}({arg})")
-      return saved[arg]
+        print(f"[cachedfunc] Using cached value for {f.__name__}{args}")
+      return saved[args]
     return newf
   return deco
 
 
 if __name__ == '__main__':
   @cachedfunc("testcache.p")
-  def myfunc(arg):
-    print(f"Calling func with arg {arg}")
-    return -arg
+  def myfunc(*args):
+    print(f"Calling func with arg {args}")
+    return -sum(args)
 
   print("f(0)=",myfunc(0))
   print("f(1)=",myfunc(1))
   print("f(0)=",myfunc(0))
+  
+  print("f(0,0)=",myfunc(0,0))
+  print("f(1,0)=",myfunc(1,0))
+  print("f(0,0)=",myfunc(0,0))

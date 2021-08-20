72b425b5ee92f6307d0cb3812cb80885a38bea2d
Victor Couty <victor@couty.eu>
2021-08-20 16:52:26 +0200

    Added Async_iter_parallel

--- async_load.py
+++ async_load.py
@@ -43,18 +43,64 @@ class Async_iter(Process):
     raise StopIteration
 
 
+class Worker(Process):
+  """
+  Super basic worker, takes data from qin,
+  applies func and puts the result in qout
+
+  Will stop if None is received
+  """
+  def __init__(self, func, qin, qout):
+    super().__init__()
+    self.func = func
+    self.qin = qin
+    self.qout = qout
+
+  def run(self):
+    while (i := self.qin.get()) is not None:
+      self.qout.put(self.func(i))
+
+
+class Async_iter_parallel(Async_iter):
+  """
+  Same idea as Async_iter, but this time using multiple loading processes
+  in parrallel.
+
+  This is only relevent if the loading times
+  are longer than the processing times
+  """
+  def __init__(self, namelist, load, processes=3, length=3, sleep_delay=.1):
+    super().__init__(namelist, load, length, sleep_delay)
+    self.np = processes
+    self.feed = Queue()
+
+  def run(self):
+    self.plist = [Worker(self.load, self.feed, self.q) for _ in range(self.np)]
+    for p in self.plist:
+      p.start()
+    for name in self.namelist:
+      while self.q.qsize() >= self.length:
+        sleep(self.sleep_delay)
+      self.feed.put(name)
+    for _ in range(self.np):
+      self.feed.put(None)
+
+
 if __name__ == '__main__':
+  from time import time
   """
   Quick demo
   """
   def load_f(name):
     print("Loading", name)
-    sleep(.2)
+    sleep(2)
     print("finished loading", name)
     return name
 
-  l = Async_iter(list(range(5)), load_f)
+  #l = Async_iter(list(range(5)), load_f)
+  l = Async_iter_parallel(list(range(5)), load_f)
 
+  t0 = time()
   for i in l:
-    print("GOT", i)
-    sleep(1)
+    print(f"[{time()-t0:.2f}] GOT", i)
+    sleep(1.1)
